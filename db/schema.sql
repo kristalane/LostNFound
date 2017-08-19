5bbef80b0c19f8bb1bf03c7debfc5995ebfad774
Zach Radford <zachradford13@gmail.com>
2017-08-19 13:17:26 -0700

    Adjusting tables/columns
    Removed need for FB key, will implement later, adjusted comments to be accurate.

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,11 @@
 CREATE DATABASE lost_and_found;
 USE lost_and_found;
 
--- Created the table "schools" 
+-- Created the table "users" 
 CREATE TABLE users (
   userid int AUTO_INCREMENT,
   name varchar(99) NOT NULL,
   item varchar NOT NULL,
-  -- Firebase key
-  fb_key varchar,
   PRIMARY KEY(id)
 );
 
