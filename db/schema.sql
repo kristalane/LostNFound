3d4e9e4e168533a25cc89e6687f5e18f7da48e86
Zach Radford <zachradford13@gmail.com>
2017-08-19 10:23:08 -0700

    Moved seed data from schema to seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -13,6 +13,3 @@ CREATE TABLE users (
 );
 
 
-INSERT INTO users (name, item, fb_key) VALUES ("Joe", "Sweatshirt", "23525252");
-INSERT INTO users (name, item, fb_key) VALUES ("Bob", "Watch", "930457");
-INSERT INTO users (name, item, fb_key) VALUES ("Fred", "Car", "09283485");
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, item, fb_key) VALUES ("Joe", "Sweatshirt", "23525252");
+INSERT INTO users (name, item, fb_key) VALUES ("Bob", "Watch", "930457");
+INSERT INTO users (name, item, fb_key) VALUES ("Fred", "Car", "09283485");
