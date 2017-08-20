758ba5d29b7ccba7c1d49f8b065452dc1cd22d5a
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-20 12:14:04 -0700

    Updating stuff model to match updated schema design; adding html routes file

--- models/item.js
+++ models/item.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-  var item = sequelize.define("lost_and_found", {
-    type: DataTypes.STRING,
+  var Stuff = sequelize.define("stuff", {
+    itemtype: DataTypes.STRING,
     size: DataTypes.STRING,
     color: DataTypes.STRING,
-    attributes: DataTypes.STRING,
+    attrib: DataTypes.STRING,
     location: DataTypes.POINT
   });
-  return item;
+  return Stuff;
 };
