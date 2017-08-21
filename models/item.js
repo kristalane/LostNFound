7777c4ee3321eb4d9022c6cbdc86b6ce6c112818
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-20 17:47:23 -0700

    adding index.js for models directory; updating item.js with flags

--- models/item.js
+++ models/item.js
@@ -1,10 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
   var Stuff = sequelize.define("stuff", {
-    itemtype: DataTypes.STRING,
-    size: DataTypes.STRING,
-    color: DataTypes.STRING,
-    attrib: DataTypes.STRING,
-    location: DataTypes.POINT
+    itemtype: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    size: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attrib: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lost: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
   return Stuff;
 };
