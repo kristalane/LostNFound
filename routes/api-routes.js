9952ae28d44f9c1e1d4bebb3d0d0414e78871599
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-21 21:04:40 -0700

    updating api routes and adding test directory

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,27 +2,29 @@ var db = require("../models");
 
 // Routes =============================================================
 module.exports = function(app) {
-
   app.get("/api/stuff", function(req, res) {
     db.Stuff.findAll({}).then(function(dbStuff) {
       res.json(dbStuff);
     });
   });
-//Why do we need this we're never calling a select all to display all
 
   app.post("/api/stuff", function(req, res) {
-    db.stuff.create({
+    db.Stuff.create({
       itemtype: req.body.itemtype,
       size: req.body.size,
       color: req.body.color,
-      attrib: req.body.attrib
+      location:req.body.location,
+      attrib: req.body.attrib,
+      lost: req.body.isLost
     }).then(function(dbStuff) {
       res.json(dbStuff);
     });
   });
 
+
+
   app.delete("/api/stuff/:id", function(req, res) {
-    db.stuff.destroy({
+    db.Stuff.destroy({
       where: {
         id: req.params.id
       }
