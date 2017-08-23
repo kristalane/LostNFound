60b359061bfd6fc04f7853ccbc9ade9d9e054903
Krista Lane <klane@calvisitor-10-105-175-60.calvisitor.1918.berkeley.edu>
2017-08-22 18:09:55 -0700

    removing completed TODOs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,23 +11,22 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
 // this is the root/home page.
-// TODO: add html file name
+
   app.get("/", function(req, res){
     console.log("here")
     res.sendFile(path.resolve(path.join(__dirname, "..", "views", "layouts", "home.html")));
   })
 
 // this route is for a user who says they lost an item - will display all found items.
-// TODO: add html file name
+
 
   app.get("/lost", function(req, res) {
     res.sendFile(path.resolve(path.join(__dirname, "..", "views", "layouts", "lost.html")))
   });
 
 // this route is for a user who says they found an item - will display all lost items
-// TODO: add html file names
+
   app.get("/found", function(req, res){
     res.sendFile(path.resolve(path.join(__dirname, "..", "views", "layouts", "found.html")))
   });
 };
-
